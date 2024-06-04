555ef14e4a701213049093af6e9058e369ba6d28
gadcab <gad.cabilo.gc@gmail.com>
2024-06-04 13:08:57 +0200

    Exercice 7

---
+++ src/increment.spec.js
@@ -0,0 +1,7 @@
+import {increment} from "./increment"
+
+test('test increment function', () => {
+    expect(increment(1)).toBe(2)
+    expect(increment(42)).toBe(43)
+    expect(increment(-42)).toBe(-41)
+})
